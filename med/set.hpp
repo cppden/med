758f3d3970d278e73456abe43e667d23d8e5c8fe
Denis Priyomov <DenisPriyomov@hotmail.com>
2018-04-08 18:44:17 +0300

    replaced set_decoder/set_encoder with set_for

--- med/set.hpp
+++ med/set.hpp
@@ -23,87 +23,6 @@ namespace med {
 
 namespace sl {
 
-/**
- * structural decoder of set
- */
-template <class... IES>
-struct set_decoder;
-
-template <class IE, class... IES>
-struct set_decoder<IE, IES...>
-{
-	template <class TO, class FUNC, class UNEXP, class HEADER>
-	static inline MED_RESULT decode(TO& to, FUNC& func, UNEXP& unexp, HEADER const& header)
-	{
-		if constexpr (is_multi_field<IE>::value)
-		{
-			if (tag_type_t<IE>::match( get_tag(header) ))
-			{
-				IE& ie = static_cast<IE&>(to);
-				CODEC_TRACE("[%s]@%zu", name<typename IE::field_type>(), ie.count());
-				if (ie.count() >= IE::max)
-				{
-					return func(error::EXTRA_IE, name<typename IE::field_type>(), IE::max, ie.count());
-				}
-				auto* field = ie.push_back(func);
-				return MED_EXPR_AND(field) med::decode(func, *field, unexp);
-			}
-		}
-		else //single-instance field
-		{
-			if (tag_type_t<IE>::match( get_tag(header) ))
-			{
-				IE& ie = static_cast<IE&>(to);
-				CODEC_TRACE("[%s] = %u", name<typename IE::field_type>(), ie.ref_field().is_set());
-				if (!ie.ref_field().is_set())
-				{
-					return med::decode(func, ie.ref_field(), unexp);
-				}
-				return func(error::EXTRA_IE, name<typename IE::field_type>(), 1);
-			}
-		}
-		return set_decoder<IES...>::decode(to, func, unexp, header);
-	}
-
-	template <class TO, class FUNC>
-	static MED_RESULT check(TO const& to, FUNC& func)
-	{
-		if constexpr (is_multi_field<IE>::value)
-		{
-			IE const& ie = static_cast<IE const&>(to);
-			MED_CHECK_FAIL(check_arity(func, ie));
-		}
-		else //single-instance field
-		{
-			IE const& ie = static_cast<IE const&>(to);
-			if (!is_optional_v<IE> && !ie.ref_field().is_set())
-			{
-				return func(error::MISSING_IE, name<typename IE::field_type>(), 1, 0);
-			}
-		}
-		return set_decoder<IES...>::check(to, func);
-	}
-};
-
-template <>
-struct set_decoder<>
-{
-	template <class TO, class FUNC, class UNEXP, class HEADER>
-	static MED_RESULT decode(TO& to, FUNC& func, UNEXP& unexp, HEADER const& header)
-	{
-		return unexp(func, to, header);
-	}
-
-	template <class TO, class FUNC>
-	static constexpr MED_RESULT check(TO&, FUNC&)       { MED_RETURN_SUCCESS; }
-};
-
-/**
- * structural encoder of set
- */
-template <class... IES>
-struct set_encoder;
-
 template <class HEADER, class IE, class FUNC>
 constexpr MED_RESULT encode_header(FUNC& func)
 {
@@ -120,9 +39,34 @@ constexpr MED_RESULT encode_header(FUNC& func)
 	}
 }
 
-template <class IE, class... IES>
-struct set_encoder<IE, IES...>
+template <class T, class FUNC>
+constexpr void pop_state(FUNC&& func)
 {
+	if constexpr (std::is_base_of<CONTAINER, typename T::ie_type>::value)
+	{
+		func(POP_STATE{});
+	}
+}
+
+template <class... IEs>
+struct set_for;
+
+template <class IE, class... IEs>
+struct set_for<IE, IEs...>
+{
+	template <typename TAG>
+	static constexpr char const* name_tag(TAG const& tag)
+	{
+		if (tag_type_t<IE>::match(tag))
+		{
+			return name<IE>();
+		}
+		else
+		{
+			return set_for<IEs...>::name_tag(tag);
+		}
+	}
+
 	template <class HEADER, class TO, class FUNC>
 	static inline MED_RESULT encode(TO const& to, FUNC& func)
 	{
@@ -160,43 +104,59 @@ struct set_encoder<IE, IES...>
 				return func(error::MISSING_IE, name<typename IE::field_type>(), 1, 0);
 			}
 		}
-		return set_encoder<IES...>::template encode<HEADER>(to, func);
+		return set_for<IEs...>::template encode<HEADER>(to, func);
 	}
-};
 
-template <>
-struct set_encoder<>
-{
-	template <class HEADER, class TO, class FUNC>
-	static constexpr MED_RESULT encode(TO&, FUNC&)       { MED_RETURN_SUCCESS; }
-};
-
-template <class T, class FUNC>
-constexpr void pop_state(FUNC&& func)
-{
-	if constexpr (std::is_base_of<CONTAINER, typename T::ie_type>::value)
+	template <class TO, class FUNC, class UNEXP, class HEADER>
+	static inline MED_RESULT decode(TO& to, FUNC& func, UNEXP& unexp, HEADER const& header)
 	{
-		func(POP_STATE{});
+		if constexpr (is_multi_field<IE>::value)
+		{
+			if (tag_type_t<IE>::match( get_tag(header) ))
+			{
+				IE& ie = static_cast<IE&>(to);
+				CODEC_TRACE("[%s]@%zu", name<typename IE::field_type>(), ie.count());
+				if (ie.count() >= IE::max)
+				{
+					return func(error::EXTRA_IE, name<typename IE::field_type>(), IE::max, ie.count());
+				}
+				auto* field = ie.push_back(func);
+				return MED_EXPR_AND(field) med::decode(func, *field, unexp);
+			}
+		}
+		else //single-instance field
+		{
+			if (tag_type_t<IE>::match( get_tag(header) ))
+			{
+				IE& ie = static_cast<IE&>(to);
+				CODEC_TRACE("[%s] = %u", name<typename IE::field_type>(), ie.ref_field().is_set());
+				if (!ie.ref_field().is_set())
+				{
+					return med::decode(func, ie.ref_field(), unexp);
+				}
+				return func(error::EXTRA_IE, name<typename IE::field_type>(), 1);
+			}
+		}
+		return set_for<IEs...>::decode(to, func, unexp, header);
 	}
-}
 
-template <class... IEs>
-struct set_for;
-
-template <class IE, class... IEs>
-struct set_for<IE, IEs...>
-{
-	template <typename TAG>
-	static constexpr char const* name_tag(TAG const& tag)
+	template <class TO, class FUNC>
+	static MED_RESULT check(TO const& to, FUNC& func)
 	{
-		if (tag_type_t<IE>::match(tag))
+		if constexpr (is_multi_field<IE>::value)
 		{
-			return name<IE>();
+			IE const& ie = static_cast<IE const&>(to);
+			MED_CHECK_FAIL(check_arity(func, ie));
 		}
-		else
+		else //single-instance field
 		{
-			return set_for<IEs...>::name_tag(tag);
+			IE const& ie = static_cast<IE const&>(to);
+			if (!is_optional_v<IE> && !ie.ref_field().is_set())
+			{
+				return func(error::MISSING_IE, name<typename IE::field_type>(), 1, 0);
+			}
 		}
+		return set_for<IEs...>::check(to, func);
 	}
 };
 
@@ -204,30 +164,39 @@ template <>
 struct set_for<>
 {
 	template <typename TAG>
-	static constexpr char const* name_tag(TAG const&)
+	static constexpr char const* name_tag(TAG const&)    { return nullptr; }
+
+	template <class HEADER, class TO, class FUNC>
+	static constexpr MED_RESULT encode(TO&, FUNC&)       { MED_RETURN_SUCCESS; }
+
+	template <class TO, class FUNC, class UNEXP, class HEADER>
+	static MED_RESULT decode(TO& to, FUNC& func, UNEXP& unexp, HEADER const& header)
 	{
-		return nullptr;
+		return unexp(func, to, header);
 	}
+
+	template <class TO, class FUNC>
+	static constexpr MED_RESULT check(TO&, FUNC&)       { MED_RETURN_SUCCESS; }
 };
 
 }	//end: namespace sl
 
 
-template <class HEADER, class ...IES>
-struct set : container<IES...>
+template <class HEADER, class ...IEs>
+struct set : container<IEs...>
 {
 	using header_type = HEADER;
 
 	template <typename TAG>
 	static constexpr char const* name_tag(TAG const& tag)
 	{
-		return sl::set_for<IES...>::name_tag(tag);
+		return sl::set_for<IEs...>::name_tag(tag);
 	}
 
 	template <class ENCODER>
 	MED_RESULT encode(ENCODER& encoder) const
 	{
-		return sl::set_encoder<IES...>::template encode<header_type>(this->m_ies, encoder);
+		return sl::set_for<IEs...>::template encode<header_type>(this->m_ies, encoder);
 	}
 
 	template <class DECODER, class UNEXP>
@@ -250,13 +219,13 @@ struct set : container<IES...>
 			}
 			sl::pop_state<header_type>(decoder);
 			CODEC_TRACE("tag=%#zx", std::size_t(get_tag(header)));
-			MED_CHECK_FAIL(sl::set_decoder<IES...>::decode(this->m_ies, decoder, unexp, header));
+			MED_CHECK_FAIL(sl::set_for<IEs...>::decode(this->m_ies, decoder, unexp, header));
 #else
 			if (med::decode(decoder, header, unexp))
 			{
 				sl::pop_state<header_type>(decoder);
 				CODEC_TRACE("tag=%#zx", std::size_t(get_tag(header)));
-				MED_CHECK_FAIL(sl::set_decoder<IES...>::decode(this->m_ies, decoder, unexp, header));
+				MED_CHECK_FAIL(sl::set_for<IEs...>::decode(this->m_ies, decoder, unexp, header));
 			}
 			else
 			{
@@ -267,7 +236,7 @@ struct set : container<IES...>
 #endif //MED_EXCEPTIONS
 		}
 
-		return sl::set_decoder<IES...>::check(this->m_ies, decoder);
+		return sl::set_for<IEs...>::check(this->m_ies, decoder);
 	}
 };
 
